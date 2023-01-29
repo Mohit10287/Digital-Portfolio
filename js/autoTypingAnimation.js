20707e4a39716e05ae46813cfa74bc608e3cd5a4
Mohit Bankey <118616357+Mohit10287@users.noreply.github.com>
2023-01-29 17:23:40 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Mohit Bankey. I hail from Harda, Madhya Pradesh currently persuing my B. Tech degree in Computer Science Engineering - final year, at Vellore Institute of Technology Bhopal University"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
